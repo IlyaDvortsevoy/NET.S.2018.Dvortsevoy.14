fcd1611ad52bdc7faf9f92083522b013aba8808a
Ilya Dvortsevoy <links309@gmail.com>
2018-07-28 14:41:18 +0300

    + Person class for unit tests added

---
+++ SearchAlgorithm.Tests/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SearchAlgorithm.Tests
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+            
+            return this.Age.CompareTo(other.Age);
+        }
+    }
+}
